60a4bca42373c41291ce52f7dcb54e54deb40ef6
TE22B-Tiilikainen-Elliot <elliot.tiilikainenvenstrom@elev.ga.ntig.se>
2024-10-08 09:18:04 +0200

    Front-end Complete

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function registerClick() {
-    fetch('http://localhost:8080/button-click', { 
-        method: 'POST' 
+    fetch('http://localhost:8080/button-click', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
-    .then((response) => response.text())
-    .then((data) => {
-        document.getElementById("countDisplay").innerText =
-          "Button pressed: " + data + " times";
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('click-count').innerText = 'Click Count: ' + data;
     })
-    .catch((error) => console.error('Error:', error));
+    .catch(error => console.error('Error:', error));
 }
